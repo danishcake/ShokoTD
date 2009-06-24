c2d308cc3b65b46d67413c898dbb098cb51cfe31
Edward Woolhouse <danishcake@hotmail.com>
2009-06-24 19:07:16 +0100

    Made building and cleaning a bit more verbose

--- BuildDebug.bat
+++ BuildDebug.bat
@@ -1,2 +1,3 @@
+@echo Building Debug
 path=%PATH%;%PROGRAMFILES%\Microsoft Visual Studio 8\Common7\IDE
 devenv.com ShokoRocket++.sln /build Debug
--- BuildRelease.bat
+++ BuildRelease.bat
@@ -1,2 +1,3 @@
+@echo Building Release
 path=%PATH%;%PROGRAMFILES%\Microsoft Visual Studio 8\Common7\IDE
 devenv.com ShokoRocket++.sln /build Release
