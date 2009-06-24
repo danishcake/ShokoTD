359c1b5b2a7cd2b2e5c1f907f6b478bfcfd28f32
Edward Woolhouse <danishcake@hotmail.com>
2009-06-24 19:35:33 +0100

    Improved cleaning batch files to remove incremental linker files and .pdb files

--- CleanDebug.bat
+++ CleanDebug.bat
@@ -1,3 +1,7 @@
 @echo Cleaning up Debug
 path=%PATH%;%PROGRAMFILES%\Microsoft Visual Studio 8\Common7\IDE
 devenv.com ShokoTD.sln /clean Debug
+@echo Removing incremental linker stuff
+del *.ilk /s
+@echo Removing debug database
+del *.pdb
--- CleanRelease.bat
+++ CleanRelease.bat
@@ -1,3 +1,7 @@
 @echo Cleaning up Release
 path=%PATH%;%PROGRAMFILES%\Microsoft Visual Studio 8\Common7\IDE
 devenv.com ShokoTD.sln /clean Release
+@echo Removing incremental linker stuff
+del *.ilk /s
+@echo Removing debug database
+del *.pdb
